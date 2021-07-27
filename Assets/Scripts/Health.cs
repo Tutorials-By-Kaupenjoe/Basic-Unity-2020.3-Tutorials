820fdfadde8395ddde58e75b4e64d67b5f1b4ae2
Kaupenjoe <N.Kaupenjohann@gmail.com>
2021-07-27 20:04:12 +0200

    refactored health script

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -4,32 +4,61 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-    public int health = 100;
-    public int mana = 50;
-    [SerializeField] private int experience = 42;
+    [SerializeField] private int health = 100;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        health -= 20;
-        Debug.Log("Current Experience " + experience); // Added so we stop getting the warning anymore
-    }
+    private int MAX_HEALTH = 100;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.D))
         {
-            health -= 20;
-            if (health <= 0)
-            {
-                Debug.Log("Lol, you died. Press F to pay respect!");
-            }
+            Damage(10);
         }
 
-        if (health <= 0 && Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.H))
         {
-            Debug.Log("RESPECT");
+            Heal(10);
         }
+    }
+
+    public void Damage(int amount)
+    {
+        if(amount < 0)
+        {
+            throw new System.ArgumentOutOfRangeException("Cannot have negative Damage");
+        }
+
+        this.health -= amount;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        if (amount < 0)
+        {
+            throw new System.ArgumentOutOfRangeException("Cannot have negative healing");
+        }
+
+        bool wouldBeOverMaxHealth = health + amount > MAX_HEALTH;
+
+        if (wouldBeOverMaxHealth)
+        {
+            this.health = MAX_HEALTH;
+        }
+        else
+        {
+            this.health += amount;
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("I am Dead!");
+        Destroy(gameObject);
     }
 }
